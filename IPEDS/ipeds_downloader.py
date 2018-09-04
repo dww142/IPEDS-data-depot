98eab3af0647324aa3a30e6d1e3db9586dbb3913
dww142 <danielwelsh@gmail.com>
2018-09-04 14:48:01 -0400

    removed data loading functions (separate script); modified logging and data path;

--- IPEDS/ipeds_downloader.py
+++ IPEDS/ipeds_downloader.py
@@ -24,84 +24,6 @@ import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 
-def table_column_metadata(file_code):
-    '''
-    Get the column names and data types from an existing SQL Server database table
-    for the specified IPEDS survey file. 
-    '''
-    # from sqlalchemy.ext.declarative import declarative_base
-    base = declarative_base()
-    base.metadata.reflect(settings.ENGINE, schema=settings.TARGET_SCHEMA)
-    cols = {}
-    if f'{settings.TARGET_SCHEMA}tblHD' in base.metadata.tables:
-        for c in base.metadata.tables[f'{settings.TARGET_SCHEMA}tblHD'].columns:
-            cols[str(c.name)] = c.type
-    else:
-        cols = None
-    return cols
-
-def compare_file_table(file_code, file_frame_columns):
-    '''
-    Find any columns that are in the provided Pandas data frame
-    that are NOT in the existing database table. 
-    
-    **These columns can either be removed from the frame to allow a clean insert;
-    Or - modify the table structure to account for the new columns. Account running
-    the script must have permission to change structure of the table. 
-    '''
-    table_columns = table_column_metadata(file_code)
-    if table_columns:
-        not_in_table = [c for c in file_frame_columns if c not in table_columns]
-    else:
-        not_in_table = None
-    # not_in_file = [c for c in table_columns if c not in file_frame.columns]
-    return not_in_table
-
-def clean_file_frame(file_frame, not_in_table):
-    '''
-    remove any columns from the file frame that are not
-    in the target database table for the given file_code**
-    TODO: make an alternate function that updates the table structure adding columns
-    '''
-    for frame_col in file_frame.columns:
-        if frame_col in not_in_table:
-            del file_frame[frame_col]
-    return not_in_table
-
-def del_current_year(file_year, file_code):
-    '''
-    Delete's data in an existing table for the specified year. 
-    Verify table exists before calling
-    '''
-    del_year = f'DELETE FROM {settings.TARGET_SCHEMA}tbl{file_code} WHERE SURVEY_YEAR = {str(file_year)}'
-    print(f'{file_year} data deleted from tbl{file_code}')
-    settings.ENGINE.execute(del_year)
-    pass
-
-def table_exists(file_code):
-    '''
-    check if target table exists in the existing database
-    '''
-    sql = f"SELECT CASE WHEN EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tbl{file_code}') THEN 'True' ELSE 'False' END"
-    result = settings.ENGINE.execute(sql)
-
-    exists = result.first()
-
-    if exists:
-        table_status = True if exists[0] == 'True' else False
-
-    return table_status
-
-def add_missing_columns_to_table(missing_columns, file_code, ENGINE):
-    '''
-    If any columns exist in dataframe that are not in table
-    adds VARCHAR(MAX) columns to table to account for data
-    '''
-    for new_col in missing_columns:
-        add_column_sql = f'ALTER TABLE {settings.TARGET_SCHEMA}tbl{file_code} ADD {new_col} VARCHAR(500) NULL'
-        settings.ENGINE.execute(add_column_sql)
-        # print(add_column_sql)
-            
 def extract_file(data, survey, file_code, survey_year, dictionary=False):
     '''
     Extract the latest data or dictionary file in the downloaded zip file to the data directory
@@ -113,7 +35,7 @@ def extract_file(data, survey, file_code, survey_year, dictionary=False):
     # if multiple files in zip, extract the last file in the list
     # Should represent the _RV suffixed file containing the latest revised data ONLY
     data_file = data.namelist()[len(data.namelist())-1]
-    output_path = os.path.join(settings.DATA_DIRECTORY, survey, file_code)
+    output_path = os.path.join(settings.DATA_DIRECTORY, survey, file_code, settings.DOWNLOAD_DATE)
     if dictionary:
         output_path = os.path.join(output_path, 'Dictionaries')
     else:
@@ -214,56 +136,6 @@ def consolidate_survey_files(file_code_log, file_code, row_test=None):
     if len(file_frames) > 0:
         return pd.concat(file_frames)
 
-def create_table(file_code, columns):
-    '''
-    Create the ETL DB table for the specified file code with varchar(500) columns. 
-    Prevents the varchar(max) columns of the default write
-    '''
-    create_sql = f'CREATE TABLE {settings.TARGET_SCHEMA}tbl{file_code}(\n'
-    for column in columns:
-        if column.upper() in ['SURVEY_YEAR','UNITID']:
-            create_sql += f'{column} INT not null'
-        else:
-            create_sql += f'{column} varchar(500) null \n'
-    pass
-def write_years_to_table(file_code, file_code_log, row_test=None):
-    '''
-    Write one file to the existing table; check if all columns exist first, add if missing
-    '''
-    for file_year in file_code_log:
-        if file_code_log[file_year] != 'File Not Found':
-            file_frame = pd.read_csv(file_code_log[file_year]
-                                     , dtype=object
-                                     , nrows=row_test
-                                     , encoding=settings.READ_ENCODING
-                                    )
-            # Cleanse string placeholders formats from IPEDS numeric data columns
-            file_frame = file_frame.replace(['.', ' .'], '')
-            file_frame['SURVEY_YEAR'] = pd.Series(file_year, file_frame.index) #year not available..
-            file_frame.columns = [col.upper().strip() for col in file_frame.columns]
-            #DATA ISSUE IN IPEDS - the IPEDS source file transposed the imputation column and 
-            # data column in this file for this year; swap column headings to correctly align:
-            if file_year == 2008 and 'F2A20' in file_frame.columns:
-                cols = []
-                for c in file_frame.columns:
-                    if c == 'XF2A20':
-                        cols.append('F2A20')
-                    elif c == 'F2A20':
-                        cols.append('XF2A20')
-                    else:
-                        cols.append(c)
-                file_frame.columns = cols
-            # 
-            if table_exists(file_code):
-                missing = compare_file_table(file_code, file_frame.columns)
-                add_missing_columns_to_table(missing, file_code, settings.ENGINE)
-                del_current_year(file_year, file_code)
-            else:
-                create_table(file_code, file_frame.columns)
-                missing = None
-            file_frame.to_sql('tbl'+file_code, settings.ENGINE, schema=settings.TARGET_SCHEMA, if_exists='append', index=False)
-            print(file_year, 'written.', 'Columns Added:', missing)
-    pass
 
 def __main__():
     '''
@@ -284,15 +156,14 @@ def __main__():
     surveys = settings.DOWNLOAD_SURVEY_FILE_LIST
     start_time = dt.datetime.now()
     #loop through the surveys you want to download
-    for survey in surveys:
-        log = {'Download Date' : settings.DOWNLOAD_DATE,
-               'Search Start Year' : settings.START_YEAR,
-               'Search End Year' : settings.END_YEAR
-              }
-        log[survey] = {}
-        
+    for survey in surveys:        
         # loop through the files for that survey
         for file_code in surveys[survey]:
+            log = {'DOWNLOAD_DATE' : settings.DOWNLOAD_DATE,
+                   'START_YEAR' : settings.START_YEAR,
+                   'END_YEAR' : settings.END_YEAR
+            }
+            log[survey] = {}
             log[survey][file_code] = {}
 
             #loop through the desired years, for that file:
@@ -301,26 +172,10 @@ def __main__():
                 meta_result = download_file(survey, file_code, survey_year, get_dictionary=settings.GET_DICTIONARIES)
                 print("Done.")
                 log[survey][file_code][survey_year] = meta_result
-            
-            if settings.WRITE_TO_TABLE:
-                write_years_to_table(file_code, log[survey][file_code])
-            
-            #once you have all years, start consolidating the separate files:
-            # print('Start Consolidation')
-            # consolidated_file = consolidate_survey_files(log[survey][file_code], file_code)
-            # print('Consolidation Done.')
 
-            ####### WRITE CONSOLIDATED FILE TO SQL
-            # consolidated_file.to_sql('tbl'+file_code, settings.ENGINE, schema=settings.TARGET_SCHEMA, if_exists=settings.APPEND_OR_REPLACE, index=False)
-            
-            # print logging:
-            print(file_code)
-            print('\t', log[survey][file_code])
-            # print('\t', len(consolidated_file.index), 'records inserted')
-
-        log_file = survey+'_log.json'
-        with open(os.path.join(settings.DATA_DIRECTORY, survey, log_file), 'w') as survey_log:
-            json.dump(log, survey_log, indent=2)
+            log_file = f'{survey}_{file_code}_{settings.DOWNLOAD_DATE}_log.json'
+            with open(os.path.join(settings.DATA_DIRECTORY, survey, file_code, settings.DOWNLOAD_DATE, log_file), 'w') as survey_log:
+                json.dump(log, survey_log, indent=2)
 
     # download_log = {settings.DOWNLOAD_DATE : log}
     # print(json.dumps(download_log, indent=2))
