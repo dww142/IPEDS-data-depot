de10fc0ab4d5481173a3cf42f159060d98776608
dww142 <danielwelsh@gmail.com>
2018-10-17 10:10:26 -0400

    update race code alignment across sources

--- SHARED/ETL_Views/SHARED.vw_DimRaceEthnicity.sql
+++ SHARED/ETL_Views/SHARED.vw_DimRaceEthnicity.sql
@@ -6,7 +6,26 @@
 		Add other mappings as necessary
 
 	TODO: Add mappings to NCES / EdFacts race code values
+
+map: 
+CENSUS_RACE_CODES = {
+    'A' : 'WHITE ALONE',
+    'B' : 'BLACK OR AFRICAN AMERICAN ALONE',
+    'C' : 'AMERICAN INDIAN AND ALASKA NATIVE ALONE',
+    'D' : 'ASIAN ALONE',
+    'E' : 'NATIVE HAWAIIAN AND OTHER PACIFIC ISLANDER ALONE',
+    'F' : 'SOME OTHER RACE ALONE',
+    'G' : 'TWO OR MORE RACES',
+    'H' : 'WHITE ALONE, NOT HISPANIC OR LATINO',
+    'I' : 'HISPANIC OR LATINO',
+
+
+****** iMPORTANT NOTE: 
+	- Each set of codes should be unique; i.e., multiple IPEDS codes should not map to the same RaceCd, 
+	multiple Census Codes should not map to the same IPEDS code, etc. 
+	The DESCRIPTIVE fields can be the same, but the join fields should be different or risk duplication in joining
 ***************************************************/
+USE OSDS_ETL;
 
 DROP VIEW IF EXISTS SHARED.vw_DimRaceEthnicityCode
 GO
@@ -22,11 +41,22 @@ CREATE VIEW SHARED.vw_DimRaceEthnicityCode AS
 					WHEN 4 THEN 'HISP'
 					WHEN 5 THEN 'WH'
 					WHEN 6 THEN 'MR'
-					WHEN 7 THEN 'WH'
 					WHEN 9 THEN 'ASN'
 					WHEN 10 THEN 'NHOPI'
-					ELSE 'NA'
+					ELSE NULL
 				END [IPEDSRaceCd]
+
+			, CASE L.LookupCd
+					WHEN 1 THEN 'C'
+					WHEN 2 THEN 'F'
+					WHEN 3 THEN 'B'
+					WHEN 4 THEN 'I'
+					WHEN 5 THEN 'H'
+					WHEN 6 THEN 'G'
+					WHEN 9 THEN 'D'
+					WHEN 10 THEN 'E'
+					ELSE NULL
+				END [CensusRaceCd]
 	FROM [SHARED].[tblLookupImport] L
 		INNER JOIN (
 						SELECT MAX(LL.[SourceYear]) LATEST_LKP_SY, LL.LookupCd 
@@ -37,10 +67,15 @@ CREATE VIEW SHARED.vw_DimRaceEthnicityCode AS
 					) LATEST ON L.[SourceYear] = LATEST.LATEST_LKP_SY AND LATEST.LookupCd = L.LookupCd
 	WHERE UPPER(L.LookupName) =  'RACE OR ETHNICITY'
 
-	/*ADD NA values and IPEDS Values to view manually */
-	UNION SELECT 'NA','NA','NA'
-	UNION SELECT 'UNK','Race or Ethnicity Unknown','UNK'
-	UNION SELECT 'NRAL','Non-Resident Alien','NRAL'
+	/*ADD NA values and IPEDS Values to view manually 
+        RaceCd, RaceDesc, IPEDSRaceCd, CensusRaceCd
+    */
+	UNION SELECT 'NA','NA','NA','NA'
+	UNION SELECT 'UNK','Race or Ethnicity Unknown','UNK','UNK'
+	UNION SELECT 'NRAL','Non-Resident Alien','NRAL','NA_C' /*IPEDS Only Code*/
+    UNION SELECT 'WH_ONLY','White Alone (Includes Hispanic)','WH_ONLY','A' /*Census Only Code*/
+
+--ORDER BY IPEDSRaceCd	
 GO
 
  /*
@@ -49,3 +84,6 @@ GO
     DROP TABLE IF EXISTS OSDS_RPT.SHARED.tblDimRaceEthnicityCode
     SELECT * INTO OSDS_RPT.SHARED.tblDimRaceEthnicityCode FROM OSDS_ETL.SHARED.vw_DimRaceEthnicityCode
     CREATE CLUSTERED COLUMNSTORE INDEX IX_RaceEthnicity_Colstore ON OSDS_RPT.SHARED.tblDimRaceEthnicityCode
+
+
+
